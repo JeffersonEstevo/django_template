ff352c2a329dd17c91f94f7e1e405b9d9064f0a9
JeffersonEstevo <jefferson.estevo@gmail.com>
2023-12-31 15:04:19 -0300

    adicionando urls dinâmicas

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,10 @@ app_name = 'blog'
 # Django URLs:
 # https://docs.djangoproject.com/en/5.0/topics/http/urls/
 urlpatterns = [
+    #path('', views.blog, name='home'),
+    #path('post/<int:id>/', views.post, name='post'),
+    #path('exemplo/', views.exemplo, name='exemplo'),
     path('', views.blog, name='home'),
-    path('post/<int:id>', views.post, name='post'),
+    path('<int:id>/', views.post, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
