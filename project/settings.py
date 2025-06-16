03362433810736dfc6c7a721b197177029da2cf9
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2025-06-16 16:00:50 -0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = var
+SECRET_KEY = var #deverá ser substituído pela SECRET KEY gerada com o comando from django.core.management.utils import get_random_secret_key #print(get_random_secret_key())  # Copie esta chave
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
